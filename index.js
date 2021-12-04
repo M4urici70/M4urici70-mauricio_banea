c68ece2328ceed68bfc7a46850aebaf010d73432
M4urici70 <89609857+M4urici70@users.noreply.github.com>
2021-12-03 23:43:41 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,13 +5,10 @@ const port = 5000
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-app.use(require('./router'));
+app.use(require('./routes'));
 
- app.use(express.static(path.join(__dirname, 'public')));
- 
+app.use(express.static(path.join(__dirname, 'public')));
 
-
- app.listen=5000
-
+app.listen(5000);
 
 console.log('SERVER corriendo en http://localhost:5000');
